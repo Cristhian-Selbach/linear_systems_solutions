07abdc6a69bd306ccab0c36fc9ef5ee79729ad53
Cristhian Selbach <crsthian.04@gmail.com>
2025-03-07 17:03:06 -0300

    matrix size

--- main.py
+++ main.py
@@ -11,27 +11,48 @@ text_font=ctk.CTkFont(family='Inter', size=14, weight="bold")
 # --------------------------------------------------------------
 
 # Patterns -----------------------------------------------------
-class std_button(ctk.CTkButton):
-  def __init__(self, master, text, command=None):
+class Button(ctk.CTkButton):
+  def __init__(self, master, text, width=140, command=None):
     super().__init__(
       master, 
       text=text,
+      width=width,
       fg_color="#D9D9D9",   
       hover_color="#8A8A8A", 
       text_color="#262626",
       corner_radius=0,
       border_width=0,
       command=command,
-      font=text_font
+      font=text_font,
     )
-# ------------------------------------------------------------
+# --------------------------------------------------------------
+
+# Functions -----------------------------------------------------
+def increase():
+  matrix_size.set(matrix_size.get() + 1)
+  matrix_label.configure(text=(matrix_size.get()))
+
+
+def decrease():
+    matrix_size = matrix_size - 1
 
 
 title_label = ctk.CTkLabel(window, text="Linear Systems Solutions", font=title_font)
 title_label.pack(pady = 20)
 
-button = std_button(window, "Test button")
-button.pack()
+matrix_size = ctk.IntVar(value=2)
+
+matrix = []
+
+matrix_label = ctk.CTkLabel(window, text="[2, 2]", font=title_font)
+matrix_label.pack(pady = 20)
+
+frame = ctk.CTkFrame(master=window, width=200, height=200)
+frame.pack()
+increase_button = Button(frame, text="+", width=30, command=increase)
+decrease_button = Button(frame, text="-", width=30)
+increase_button.pack(side="left")
+decrease_button.pack(side="left")
 
 
 
