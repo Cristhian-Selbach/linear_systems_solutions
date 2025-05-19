90fbfcfb4c16d6602f547bac0f0cffa9e579ca61
Cristhian Selbach <crsthian.04@gmail.com>
2025-05-19 16:50:55 -0300

    new row in gauss method

--- gauss.py
+++ gauss.py
@@ -2,29 +2,30 @@ import time
 
 matrix = [
   [2, 3, 12],
-  [4, -1, 5]
+  [4, -1, 5],
 ]
 
 def gauss_method(matrix, matrix_size):
-
   pivot = matrix[0][0]
   multipliers = []
+  operations_matrix = matrix
 
   for i in range(matrix_size - 1):
-    multiplier = (matrix[i+1][0] / pivot)
+    multiplier = matrix[i+1][0] / pivot
     multipliers.append(multiplier)
 
-  operations_matrix = matrix #change this later
-  l2 = [0] * (matrix_size + 1)
-
   for i in range(matrix_size - 1):
+    new_row = [0] * (matrix_size + 1)
 
     for j in range(matrix_size + 1):
-      l2[j] = matrix[i+1][j] - (multipliers[i] * matrix[i][j])
-    
+      new_row[j] = matrix[i+1][j] - multipliers[i] * matrix[0][j]
+
+    # update new line
+    operations_matrix[i+1] = new_row
 
   print("multipliers:", multipliers)
-  print("l2:", l2)
+  print("matriz após eliminação:")
+  for row in operations_matrix:
+    print(row)
 
 gauss_method(matrix, 2)
-  
