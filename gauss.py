cc6af72f8a164e25f8cf1c544c6950812bc69d94
Cristhian Selbach <crsthian.04@gmail.com>
2025-05-20 17:00:16 -0300

    corrections to the gauss algorithm

--- gauss.py
+++ gauss.py
@@ -1,31 +1,45 @@
-import time
 
+
+
+
+
+
+
+
+
+
+def gauss_method(matrix, matrix_size):
+  operations_matrix = []
+  for row in matrix:
+    operations_matrix.append(row[:]) # copy of original matrix
+
+  for i in range(matrix_size - 1):
+    pivot = operations_matrix[i][i]
+
+    for j in range(i + 1, matrix_size): # starts on the line below the pivot
+      multiplier = operations_matrix[j][i] / pivot
+
+      for k in range(matrix_size + 1):
+        operations_matrix[j][k] -= multiplier * operations_matrix[i][k]
+
+  print("matrix:")
+  for row in operations_matrix:
+    print(row)
+
+
+# matrix = [
+#   [2, 1, -1, 8],
+#   [-3, -1, 2, -11],
+#   [-2, 1, 2, -3],
+# ]
+# matrix = [
+#   [3, 2, 4, 1],
+#   [1, 1, 2, 2],
+#   [4,3, -2, 3],
+# ]
 matrix = [
   [2, 3, 12],
   [4, -1, 5],
 ]
-
-def gauss_method(matrix, matrix_size):
-  pivot = matrix[0][0]
-  multipliers = []
-  operations_matrix = matrix
-
-  for i in range(matrix_size - 1):
-    multiplier = matrix[i+1][0] / pivot
-    multipliers.append(multiplier)
-
-  for i in range(matrix_size - 1):
-    new_row = [0] * (matrix_size + 1)
-
-    for j in range(matrix_size + 1):
-      new_row[j] = matrix[i+1][j] - multipliers[i] * matrix[0][j]
-
-    # update new line
-    operations_matrix[i+1] = new_row
-
-  print("multipliers:", multipliers)
-  print("matriz após eliminação:")
-  for row in operations_matrix:
-    print(row)
 
 gauss_method(matrix, 2)
